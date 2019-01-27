8d93b4df0c7f25933ed764e0da0c9dbf8f8f7a2c
swirlflag <swirlflag>
2019-01-27 19:47:24 +0900

    up

---
+++ 2019/vue/middle/vue-chart/chart-lib/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>chart.js~~~</h1>
+    <bar-chart></bar-chart>
+    <br>
+    <line-chart></line-chart>
+  </div>
+  
+</template>
+
+<script>
+
+import BarChart from './components/BarChart.vue';
+import LineChart from './components/LineChart.vue';
+
+export default {
+  
+  components : {
+    'bar-chart' : BarChart,
+    'line-chart' : LineChart,
+  },
+
+};
+
+</script>
+
+<style>
+
+</style>
